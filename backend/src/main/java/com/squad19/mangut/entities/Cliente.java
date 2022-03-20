8faf779c713544afb3f67f1a5460d8f0314b6c6b
DAIANE LINHARES FERREIRA <daialinhares26@gmail.com>
2022-03-20 13:54:44 -0300

    atualizacao entity cliente

--- backend/src/main/java/com/squad19/mangut/entities/Cliente.java
+++ backend/src/main/java/com/squad19/mangut/entities/Cliente.java
@@ -9,18 +9,33 @@ public class Cliente {
 
 	private Long id;
 	private String nome;
+	private String telefone;
 	private String email;
 	private String senha;
+	private String logradouro;
+	private String numeroLogradouro;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
 
 	private Cliente() {
 
 	}
 
-	public Cliente(Long id, String nome, String email, String senha) {
+	public Cliente(Long id, String nome, String telefone, String email, String senha, String logradouro,
+			String numeroLogradouro, String bairro, String cidade, String estado, String cep) {
 		this.id = id;
 		this.nome = nome;
+		this.telefone = telefone;
 		this.email = email;
 		this.senha = senha;
+		this.logradouro = logradouro;
+		this.numeroLogradouro = numeroLogradouro;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
 	}
 
 	public Long getId() {
@@ -39,6 +54,14 @@ public class Cliente {
 		this.nome = nome;
 	}
 
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	public String getEmail() {
 		return email;
 	}
@@ -53,6 +76,54 @@ public class Cliente {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumeroLogradouro() {
+		return numeroLogradouro;
+	}
+
+	public void setNumeroLogradouro(String numeroLogradouro) {
+		this.numeroLogradouro = numeroLogradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 }
