9d4013fb27f06d76780e0c742adce2fdbf5cf6b6
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-03-20 16:25:11 -0300

    Cliente(Post, Put, Delete)

--- backend/src/main/java/com/squad19/mangut/resources/ClienteController.java
+++ backend/src/main/java/com/squad19/mangut/resources/ClienteController.java
@@ -1,9 +1,14 @@
 package com.squad19.mangut.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,23 @@ public class ClienteController {
 	@GetMapping
 	public List<Cliente> findAll() {
 		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+    public Optional<Cliente> findById(@PathVariable Long id) {
+        Optional<Cliente> clienteId = service.findById(id); 
+        return clienteId;
+    }
+		
+	@PostMapping    
+    public Cliente create(@RequestBody Cliente cliente) {
+        return service.create(cliente);
+    }	
+	
+	
+	@DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);        
 	}
 	
 }
--- backend/src/main/java/com/squad19/mangut/services/ClienteService.java
+++ backend/src/main/java/com/squad19/mangut/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.squad19.mangut.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,4 +21,20 @@ public class ClienteService {
 		List<Cliente> resultado = repository.findAll();
 		return resultado;
 	}
+	
+	@Transactional(readOnly = true)
+	public Optional<Cliente> findById(Long id) {
+		return repository.findById(id);
+	}
+
+	@Transactional(readOnly = true)
+	public Cliente create(Cliente cliente) {
+		return repository.save(cliente);
+	}
+
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
 }
