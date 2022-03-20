a9aa0acf8da0a7db7a8bd798af00270ed2c79d34
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-03-20 12:24:48 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/squad19/mangut/MangutApplication.java
@@ -0,0 +1,13 @@
+package com.squad19.mangut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangutApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/squad19/mangut/MangutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad19.mangut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
