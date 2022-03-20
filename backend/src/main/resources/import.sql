de7ad6d091b6f2b05d5a93c33603c0c640deb2d1
DAIANE LINHARES FERREIRA <daialinhares26@gmail.com>
2022-03-20 16:04:15 -0300

    correcao teste banco

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,7 +1,9 @@
-INSERT INTO tb_cliente(nome, telefone, email, senha, logradouro, numeroLogradouro, bairro, cidade, estado, cep) VALUES ('Maria', '987789887', 'maria@gmail.com', '1234567', 'rua paixao rei', '78', 'veneza', 'mogi', 'SP','36300999');
+INSERT INTO tb_categoria(tipo) VALUES ('fruta');
 
-INSERT INTO tb_produto(nome, preco, quantidade) VALUES ('laranja', '14', '3');
+INSERT INTO tb_vendedor(nome, telefone, email, senha, logradouro, numero_logradouro, bairro, cidade, estado, cep) VALUES ('Frederico', '0767899999', 'fred@gmail.com', 'asdsad7', 'rua pitolomeu', '171', 'boteco', 'bh', 'MG','36444444');
 
-INSERT INTO tb_vendedor(nome, telefone, email, senha, logradouro, numeroLogradouro, bairro, cidade, estado, cep) VALUES ('Frederico', '0767899999', 'fred@gmail.com', 'asdsad7', 'rua pitolomeu', '171', 'boteco', 'bh', 'MG','36444444');
+INSERT INTO tb_cliente(nome, telefone, email, senha, logradouro, numero_logradouro, bairro, cidade, estado, cep) VALUES ('Maria', '987789887', 'maria@gmail.com', '1234567', 'rua paixao rei', '78', 'veneza', 'mogi', 'SP','36300999');
 
-INSERT INTO tb_cesta(quantidade, total) VALUES ('3', '88');
+INSERT INTO tb_produto(nome, preco, quantidade, categoria_id, vendedor_id) VALUES ('laranja', '14', '3', 1, 1);
+
+INSERT INTO tb_cesta(quantidade, total, produto_id, cliente_id) VALUES ('3', '88', 1, 1);
