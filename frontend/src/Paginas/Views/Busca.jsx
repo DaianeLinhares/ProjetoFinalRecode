38ba5dadf49e286a76f090c54f0225f34c752809
Lohane <lohanedafnevieira@gmail.com>
2022-03-20 21:14:40 -0300

    Front

---
+++ frontend/src/Paginas/Views/Busca.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () =>{
+    return (
+        
+        <div className>
+        <h1 className="text-danger text-center"><br/>Na região</h1>
+        <br/>
+        <p className="text-center">Desculpa, mas essa função não está disponível para uso no momento.</p>
+        </div>
+    );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Paginas/Views/Home';
+ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/Busca" element={<Busca />}></Route>
+        <Route path="/Produtos" element={<Produtos />}></Route>
+        <Route path="/New" element={<New />}></Route>
+        <Route path="/QuemSomos" element={<QuemSomos />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
