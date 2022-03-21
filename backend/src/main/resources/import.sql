b538965a4c902515aad1cc5fafc68bbce412d6c2
DAIANE LINHARES FERREIRA <daialinhares26@gmail.com>
2022-03-21 14:25:32 -0300

    atualizacao import produto teste

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -8,6 +8,6 @@ INSERT INTO tb_vendedor(nome, telefone, email, senha, logradouro, numero_logrado
 
 INSERT INTO tb_cliente(nome, telefone, email, senha, logradouro, numero_logradouro, bairro, cidade, estado, cep) VALUES ('Maria', '987789887', 'maria@gmail.com', '1234567', 'rua paixao rei', '78', 'veneza', 'mogi', 'SP','36300999');
 
-INSERT INTO tb_produto(nome, preco, quantidade, categoria_id, vendedor_id) VALUES ('laranja', '14', '3', 1, 1);
+INSERT INTO tb_produto(nome, preco, quantidade, categoria_id, vendedor_id, foto) VALUES ('laranja', '14', '3', 1, 1, 'https://media.gazetadopovo.com.br/2021/09/22094114/banana-descascada.jpg');
 
 INSERT INTO tb_cesta(quantidade, total, produto_id, cliente_id) VALUES ('3', '88', 1, 1);
