015aead9c17179f629d97dfc5407c4a36efcc3eb
Lohane <lohanedafnevieira@gmail.com>
2022-03-23 15:22:23 -0300

    FormContato

--- frontend/src/Components/FormContato/FormContato.jsx
+++ frontend/src/Components/FormContato/FormContato.jsx
@@ -1,32 +1,51 @@
+import { useRef } from 'react'
+import api from '../../services/api'
+import 'bootstrap/dist/css/bootstrap.css';
 
+const Formulario = () => {
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+    const nome = useRef()
+    const telefone = useRef()
+    const email = useRef()
+    const mensagem = useRef()
+
+    function enviarDados(event) {
+        event.preventDefault()
+        api.post("/contatos/salvar/", {
+            nome: nome.current.value,
+            telefone: telefone.current.value,
+            email: email.current.value,
+            mensagem: mensagem.current.value,
+        }).then((res) => console.log(res.data)).catch((err) => console.log(err))
+    }
+
   return (
     <>
        <main class="vh-100 ">
-        <section class="container col-8">
-        <h1 class="display-3 mt-4">Entre em contato</h1>
+       <form className="col-8 container" onSubmit={enviarDados}>
+                    <h1 className="display-3 mt-4">Entrar em Contato</h1>
+
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Nome</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome Completo" />
+                <input type="email" className="form-control" ref={nome} id="exampleFormControlInput1" placeholder="Nome Completo" />
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Telefone</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(21) 99777-8888" />
+                <input type="email" className="form-control" ref={telefone} id="exampleFormControlInput1" placeholder="(21) 99777-8888" />
             </div>
             
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com" />
+                <input type="email" className="form-control" ref={email} id="exampleFormControlInput1" placeholder="nome@exemplo.com" />
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Mensagem</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea className="form-control" ref={mensagem} id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-        </section>
+        </form>
 
     </main>
     </>
   );
 }
+export default Formulario;
