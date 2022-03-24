c6a06f25c502b52543ce94557c1afcbb7c54bee3
Wellington N <42657376+Bryceed@users.noreply.github.com>
2022-03-23 21:08:17 -0300

    Design update: 3
    - Footer corrigido (Ou deixava tudo branco, ou uma cor de destaque, laranja e verde ficou estranho então deixei o roxinho do squad mesmo)
    - Fixs de texto e posicionamento

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -20,7 +20,8 @@ export default () => {
                         </div>
                         <div className="col-auto d-flex flex-column flex-grow-0 justify-content-center align-content-around align-self-center flex-wrap align-items-lg-center">
                             <img className="d-lg-flex justify-content-lg-center align-items-lg-center footer4" src={squad19} />
-                            <p className="text-center text-white d-flex d-xxl-flex justify-content-center align-items-center mx-auto justify-content-md-center align-items-md-center align-items-lg-center justify-content-xxl-center align-items-xxl-center footer5">Squad</p>
+                            <p className="text-center d-flex d-xxl-flex justify-content-center align-items-center mx-auto justify-content-md-center align-items-md-center align-items-lg-center justify-content-xxl-center align-items-xxl-center footer5">Squad</p>
+                            <p class="text-center footer7">Projeto estudantil</p>
                             <img src="https://drive.google.com/uc?export=download&id=15PqOg4V4KZ6ZxCI5elhlfOIE2WmCNbvh" class="recodeLogo"/>
                              <div className="w-100">
                             </div>
